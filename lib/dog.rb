88d0dcd5fc3130e404c94e63669829323f0e7fb1
Alex Siler <davidalexsiler@gmail.com>
2019-06-05 00:25:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,8 @@ class Dog
   def name=(name)
     @name = name
   end
-end
-
-class Dog 
-   def breed 
-    breed = ('Beagle')
+  def breed 
+    breed = ('beagle')
   end
   def breed=(breed)
     @breed = breed
