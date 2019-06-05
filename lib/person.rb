b6ef3e3df7d510aa52a9749d0a3edc3562df523d
Alex Siler <davidalexsiler@gmail.com>
2019-06-05 00:27:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name 
+    name = ('beyonce')
+  end
+  def name=(name)
+    @name = name
+  end
+  def job 
+    job = ('Beagle')
+  end
+  def job=(job)
+    @job= job
+  end
+end
